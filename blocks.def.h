0fc202c262ebbd62927d1b1ce5de438d0e3358f4
Ellie <92484654+elliehelisky@users.noreply.github.com>
2022-06-18 04:29:22 +0000

    Update blocks.def.h

--- blocks.def.h
+++ blocks.def.h
@@ -3,7 +3,7 @@
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
 
-    {"^c#ffc000^ ", "/home/elh/.config/custom-scripts/weather.sh", 30, 0},
+    {"^c#ffc000^ ", "curl -s wttr.in/Chicago | sed -e 's/\x1b\[[0-9;]*m//g' -e '4!d' -e 's/[^+]* //' -e 's/ //g'", 30, 0},
     {"^c#2986cc^ ", "pamixer --get-volume", 1, 0},
     {"^c#93c47d^ ", "free -h | awk '/^Mem/ { print $3\"/\" $2 }' | sed s/i//g",
      1, 0},
